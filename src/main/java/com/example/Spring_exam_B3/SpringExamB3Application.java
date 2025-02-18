eb13f81ee155e6eb94291e38f5f81a5a06714bd4
ArthurMns <arthur.mounissens@gmail.com>
2025-02-18 09:52:21 +0100

    init

---
+++ src/main/java/com/example/Spring_exam_B3/SpringExamB3Application.java
@@ -0,0 +1,13 @@
+package com.example.Spring_exam_B3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExamB3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExamB3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_exam_B3/SpringExamB3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_exam_B3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamB3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
