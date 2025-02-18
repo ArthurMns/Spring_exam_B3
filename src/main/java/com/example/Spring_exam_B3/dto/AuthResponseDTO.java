9881789d2f0453778235ce3a87308a476ac49526
ArthurMns <arthur.mounissens@gmail.com>
2025-02-18 17:21:02 +0100

    update

---
+++ src/main/java/com/example/Spring_exam_B3/dto/AuthResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.Spring_exam_B3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponseDTO {
+    private String token;
+    private String username;
+
+}
